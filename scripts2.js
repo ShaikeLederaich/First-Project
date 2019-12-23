f4706f4724b0473684f56f46be57cfafd070c2d6
Shai <kingpepg4@gmail.com>
2019-12-23 12:13:09 +0200

    added local storage work perfect set&get

--- scripts2.js
+++ scripts2.js
@@ -3,14 +3,18 @@ const myForm = document.querySelector('#my-form');
 const submit = document.querySelector('submit');
 const submitBox = document.querySelector('#submitBox');
 
-const tasks = [];
-
+let tasks;
 
 let formError = false;
 
 myForm.addEventListener('submit', checkForm);
 myForm.addEventListener('submit', updateBoardTask);
 //FUNC--------------------All Function---------------------------//
+
+getItemsFromLocalStorage();
+
+console.log(tasks);
+
 
 function checkForm(e) {
 
@@ -58,28 +62,47 @@ function updateBoardTask(e) {
   if (formError) {
     return false;
   }else {
-    let newTaskEntered = document.getElementById('addNewTask').value;
-    let dateEntered = document.getElementById('taskDateToEnd').value;
-    let timeEntered = document.getElementById('taskTimeToEnd').value;
-    let taskDateAndTime = new Date(dateEntered +'T'+timeEntered);
-  
-    console.log(newTaskEntered);
-    console.log(taskDateAndTime);
-  
-    getTaskDateAndTime(newTaskEntered, taskDateAndTime);
+    
+    createNewTaskObj();
+    
 
     e.preventDefault();
   }
 }
+function createNewTaskObj() {
 
-function getTaskDateAndTime(newTaskValue, var_of_date_include_time) {
+  let newTaskEntered = document.getElementById('addNewTask').value;
+  let dateEntered = document.getElementById('taskDateToEnd').value;
+  let timeEntered = document.getElementById('taskTimeToEnd').value;
+  let taskDateAndTime = new Date(dateEntered +'T'+timeEntered);
+
+  // getTaskDate(taskDateAndTime);
+  // getTaskTime(taskDateAndTime);
+
+  // console.log(getTaskDate(taskDateAndTime));
+  // console.log(getTaskTime(taskDateAndTime));
+
+  // addNewTask(newTaskEntered, getTaskDate(taskDateAndTime), getTaskTime(taskDateAndTime));
+
+  // setItemsToLocalStorage(addNewTask( newTaskEntered, getTaskDate(taskDateAndTime), getTaskTime(taskDateAndTime)));
+
+  console.log(addNewTask(newTaskEntered, getTaskDate(taskDateAndTime), getTaskTime(taskDateAndTime)));
+
+  
+}
+
+function getTaskDate(var_of_date_include_time) {
 
   let dayAtMonth = var_of_date_include_time.getDate();
   let month = var_of_date_include_time.getMonth() + 1;
   let year = var_of_date_include_time.getFullYear();
 
   let taskDate = dayAtMonth + '/' + month + '/' + year;
-  console.log(taskDate);
+  // console.log(taskDate);
+  return taskDate;
+}
+
+function getTaskTime(var_of_date_include_time) {
 
   let hh = var_of_date_include_time.getHours();
   let mm = var_of_date_include_time.getMinutes();
@@ -88,10 +111,10 @@ function getTaskDateAndTime(newTaskValue, var_of_date_include_time) {
   }
   
   let taskTime = hh + ':' + mm;
-  console.log(taskTime);
-
-  addNewTask(newTaskValue, taskDate, taskTime);
+  // console.log(taskTime);
+  return taskTime;
 }
+
 
 function addNewTask(taskContent, taskDate, taskTime) {
 
@@ -101,35 +124,54 @@ function addNewTask(taskContent, taskDate, taskTime) {
     taskDeadlineTime: `${taskTime}`
   }
 
-  // console.log(newTask);
-  setItemsToLocalStorage(newTask);
 
   // tasks.push(newTask);
+
+  setItemsToLocalStorage(newTask);
+
   // console.log(tasks);
-  // e.preventDefault();
+
+  return newTask;
+}
+
+function getItemsFromLocalStorage() {
+
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+    // console.log('Avi')
+  }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
 }
 
 function setItemsToLocalStorage(newTask) {
 
-  // localStorage.setItem('tasks', JSON.stringify(newTask));
-  console.log(newTask);
-  // let tasks;
+  // localStorage.removeItem('tasks');
+  let tasks;
 
-  if(localStorage.getItem('tasks') === null) {
+  if (localStorage.getItem('tasks') === null) {
     tasks = [];
-  } else {
-    tasks = JSON.parce(localStorage.getItem('tasks'));
+  }else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
   }
+  
   tasks.push(newTask);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
   
   console.log(tasks);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  console.log(localStorage);
 
-  alert('Task Saved');
+  // alert('Task Saved'); 
+  clearFields();
   
 }
+function clearFields() {
 
-
+  document.getElementById('addNewTask').value = '';
+  document.getElementById('taskDateToEnd').value = '';
+  document.getElementById('taskTimeToEnd').value = '23:59';
+}
 
 
 
